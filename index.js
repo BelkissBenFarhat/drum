9c864a3564e564ffead1cd9ea2486c9730f3bafa
Belkiss Ben Farhat <74840164+BelkissBenFarhat@users.noreply.github.com>
2021-11-11 23:25:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var numberOfButtons = document.querySelectorAll(".drum").length
 let keyMap = {
-    w: "sounds/crash.mp3",
-    a: "sounds/kick-bass.mp3",
-    s: "sounds/snare.mp3",
-    d: "sounds/tom-1.mp3",
-    j: "sounds/tom-2.mp3",
-    k: "sounds/tom-3.mp3",
-    l: "sounds/tom-4.mp3"
+    w: "crash.mp3",
+    a: "kick-bass.mp3",
+    s: "snare.mp3",
+    d: "tom-1.mp3",
+    j: "tom-2.mp3",
+    k: "tom-3.mp3",
+    l: "tom-4.mp3"
 }
 
 function playSoundFromLetter(key) {
@@ -32,54 +32,4 @@ addEventListener("keydown", keySound)
 for (i = 0; i < numberOfButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", clickSound)
 }
-    
-/* MY WAY 
-//create array that has all buttons
-var array = document.getElementsByTagName("button")
-//iterate on all of the elements
-
-for (i=0; i<array.length; i++){
-    array[i].addEventListener("click", function(){
-        alert("I got clicked")
-        })
-        
-    }*/
-    
-
-    //     switch(buttonInnerHTML){
-    //         case "w":
-    //             var crash = new Audio("sounds/crash.mp3")
-    //             crash.play();
-    //         break;
-    
-    //         case "a":
-    //             var kick = new Audio("sounds/kick-bass.mp3")
-    //             kick.play();
-    //         break;
-    //         case "s":
-    //             var snare = new Audio("sounds/snare.mp3")
-    //             snare.play();
-    //         break;
-    
-    //         case "d":
-    //             var tom1 = new Audio("sounds/tom-1.mp3")
-    //             tom1.play();
-    //         break;
-    //         case "j":
-    //             var tom2 = new Audio("sounds/tom-2.mp3")
-    //             tom2.play();
-    //         break;
-    
-    //         case "k":
-    //             var tom3 = new Audio("sounds/tom-3.mp3")
-    //             tom3.play();
-    //         break;
-    
-    //         case "l":
-    //             var tom4 = new Audio("sounds/tom-4.mp3")
-    //             tom4.play();
-    //         break;
-    //     }
-        
-        
-    // }
+  
